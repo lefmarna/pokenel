6956d39e9f20726c45a9962db83d7418158d62df
lefmarna <panume5ya@gmail.com>
2022-02-19 14:13:10 +0900

    lintの実行

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,22 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "react-app",
-    "react-app/jest",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   root: true,
   rules: {},
-};
+}
